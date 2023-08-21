39f2930663f8b040a053e98d1a6c4759e915c323
sam kariuki <samuel.kariuki1@student.moringaschool.com>
2023-08-21 09:03:52 +0300

    revisit lab

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
 
 class Book:
+    def _init_(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+if _name_ == "_main_":
+    # You can add some code here to test the Book class directly if needed
     pass
-    
-        
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 class Shoe:
+    def _init_(self, brand, size):
+        self.brand = brand
+        self.size = size
+        self.condition = "New"  # Initialize condition as "New"
+
+    def cobble(self):
+        # Repair the shoe
+        self.condition = "New"
+        print("Your shoe is as good as new!")
+
+if _name_ == "_main_":
+    # You can add some code here to test the Shoe class directly if needed
     pass
